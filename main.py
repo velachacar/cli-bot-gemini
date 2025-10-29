d319a278ba6a4c344645652b074e78d54b882d61
Carlos Vela Chávez <velachacar@gmail.com>
2025-10-29 10:54:37 +0100

    input as cli argv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,10 +9,14 @@ def main():
     
     client = genai.Client(api_key=api_key)
     
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+    if(len(sys.argv)>1):
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001',
+            contents=sys.argv[1]
+        )
+    else:
+        print('incorrect arguments')
+        sys.exit(1)
 
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
