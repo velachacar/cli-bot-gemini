626f73faa3252a49060d7c4dfdcf11c333270f28
Carlos Vela Chávez <velachacar@gmail.com>
2025-10-29 11:20:18 +0100

    added --verbose flag

--- main.py
+++ main.py
@@ -2,25 +2,39 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get('GEMINI_API_KEY')
-    
     client = genai.Client(api_key=api_key)
+    args = sys.argv[1:]
+    verbose = False
+
+    if '--verbose' in args:
+        args.remove('--verbose')
+        verbose = True
     
-    if(len(sys.argv)>1):
+    if len(args):
+        user_prompt = sys.argv[1]
+
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+
         response = client.models.generate_content(
             model='gemini-2.0-flash-001',
-            contents=sys.argv[1]
+            contents=messages,
         )
     else:
         print('incorrect arguments')
         sys.exit(1)
 
+    if(verbose):
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     
 if __name__ == "__main__":
